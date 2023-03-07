2023a0f253409480a748d09ae416da3b6f0d5183
Preet bhardwaj <77205201+LitPreet@users.noreply.github.com>
2023-03-07 23:20:02 +0530

    Update hastag_generator.js

--- hastag_generator.js
+++ hastag_generator.js
@@ -1,4 +1,4 @@
-//hastag generator js
+//hastag generator .js
 function generateHashtag (str) {
   if(str.trim() === '') return false;
     const stringToCamel = str.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join('')
