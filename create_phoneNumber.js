794d81ea762f37177d796104c3190a41e7bc9457
Preet bhardwaj <77205201+LitPreet@users.noreply.github.com>
2023-08-18 22:12:39 +0530

    Update create_phoneNumber.js

--- create_phoneNumber.js
+++ create_phoneNumber.js
@@ -1,4 +1,4 @@
-//createphone number js program 
+//createphone number js program j
 function createPhoneNumber(numbers){
     return `(${numbers.slice(0,3).join('')}) ${numbers.slice(3,6).join('')}-${numbers.slice(6).join('')}`
 }
